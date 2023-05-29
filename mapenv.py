5d8a1d8929d9596e26577dcc3ba1998f8df52f33
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2023-05-30 00:12:00 +0200

    Add files via upload

--- mapenv.py
+++ mapenv.py
@@ -1,3 +1,4 @@
+import re
 import os
 import zlib
 import struct
@@ -91,14 +92,21 @@ def compile_mapenv(file_path):
     key_stack = []  # Stack to keep track of nested PTREE keys
     tags = []  # List to store encountered tags
     
-    file_data = bytes(16)  # Placeholder for file data
+    file_data = bytes()  # Placeholder for file data
     text_header = pack_variable('MAP_ENV') + b"\x00"  # Text header with 'MAP_ENV'
     lines = codecs.open(input_file, "r", "shift-jis").readlines()  # Read lines from input file
+    
+    entries_count = 0
+    text_length = len(text_header)
+    text_line_number = 1
     
     for line in lines:
         line = line.strip()
         
         if line.startswith("PTREE"):
+            entries_count += 1
+            text_line_number += 1
+            
             if 'PTREE' not in tags: 
                 tags.append('PTREE')  # Add PTREE tag to the list
                 
@@ -110,15 +118,19 @@ def compile_mapenv(file_path):
             
             if key.startswith("MAP_ENV"):
                 text_header += pack_variable(key.replace('MAP_ENV ', '')) + b"\x00"  # Pack variable and add to text header
+                text_length = len(text_header)
                 file_data += struct.pack("<H", 2)[:2] + b"\xFF\xFF"  # Pack type (2) and padding
                 file_data += struct.pack("<I", 0)  # Pack text offset (0)
             else:
                 text_header += pack_variable(key) + b"\x00"  # Pack variable and add to text header
+                text_length = len(text_header)
                 file_data += struct.pack("<H", 1)[:2] + b"\xFF\xFF"  # Pack type (1) and padding
                 
             file_data += struct.pack("<I", text_offset)  # Pack text offset
             
         elif line == "_PTREE;":
+            entries_count += 1
+            
             if '_PTREE' not in tags: 
                 tags.append('_PTREE')  # Add _PTREE tag to the list
                 
@@ -128,10 +140,14 @@ def compile_mapenv(file_path):
             file_data += b"\x00\xFF\xFF\xFF"  # Pack padding
             
         elif line.startswith("PTVAL") and not line.startswith("PTVALS"):
+            entries_count += 1
+            
             if 'PTVAL' not in tags: 
                 tags.append('PTVAL')  # Add PTVAL tag to the list
                 
-            values = line[6:][:-1].replace('"', '').split(',')  # Extract PTVAL values
+            values = line[6:][:-1]
+            values = re.sub(r",\s+", ",", values).replace('"', '').split(',')  # Extract PTVAL values
+            print(values)
             
             if len(values) == 1:
                 value = convert_to_type(values[0])  # Convert value to appropriate type
@@ -140,8 +156,10 @@ def compile_mapenv(file_path):
                 file_data += pack_variable(value)  # Pack value as variable
                 
             else:
+                text_line_number += 1
                 text_offset = len(text_header)  # Calculate text offset
                 text_header += pack_variable(values[1]) + b"\x00"  # Pack variable and add to text header
+                text_length = len(text_header)
                 value = convert_to_type(values[0])  # Convert value to appropriate type
                 file_data += struct.pack("<I", zlib.crc32('PTVAL'.encode()))  # Calculate CRC32 and pack it
                 file_data += b"\x02\x01\xFF\xFF"  # Pack type (2), padding, and text offset
@@ -149,6 +167,8 @@ def compile_mapenv(file_path):
                 file_data += struct.pack("<I", text_offset)  # Pack text offset
                 
         elif line.startswith("PTVALS"):
+            entries_count += 1
+            
             if 'PTVALS' not in tags: 
                 tags.append('PTVALS')  # Add PTVALS tag to the list
                 
@@ -162,8 +182,11 @@ def compile_mapenv(file_path):
                 file_data += pack_variable(value)  # Pack value as variable
     
     file_data += b"\x00"  # Add null terminator
-    file_data = align_bytes(file_data, 0xFF, 16) + text_header + b"\x00"  # Align bytes, add text header and null terminator
-    file_data = align_bytes(file_data, 0xFF, 16)  # Align bytes again
+    file_data = align_bytes(file_data, 0xFF, 16) # Align bytes
+    text_header = text_header + b"\x00" # Add null terminator
+    text_header = align_bytes(text_header, 0xFF, 16)  # Align bytes again
+    file_header = struct.pack("<I", entries_count) + struct.pack("<I", len(file_data) + 16) + struct.pack("<I", len(text_header)) + struct.pack("<I", text_line_number)
+    file_data = file_header + file_data + text_header
     file_data += create_entries_header(tags)  # Create entries header
     
     return file_data
@@ -215,7 +238,7 @@ def decompile_mapenv(file_path):
                 file.read(4)
                 text_offset = struct.unpack("<I", file.read(4))[0]
                 text, index = get_text_from_bytes(text_data, text_offset)
-                text_output += ' ' * (indent_level * 4) + 'PTREE "MAPENV","' + text + '";\n'
+                text_output += ' ' * (indent_level * 4) + 'PTREE "MAP_ENV","' + text + '";\n'
                 indent_level += 1  # Increment the indentation level
             else:
                 text_offset = struct.unpack("<I", file.read(4))[0]
